fcad98b6c11a9420d4e346729475a3428702f37e
MariaIsabel0711 <maria.isabeels02@gmail.com>
2025-08-15 15:41:06 -0500

    initial commit

---
+++ RTSM/RTSM/src/main/java/app/RtsmApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtsmApplication.class, args);
+	}
+
+}
